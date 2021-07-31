24da5cb5edae21b81ac49ba42b95ed7429caaa44
mkldrr <mikail.drr@gmail.com>
2021-07-31 11:47:28 -0500

    ExcelReview, create excel file

--- src/test/java/com/cybertek/tests/reviews/Users.java
+++ src/test/java/com/cybertek/tests/reviews/Users.java
@@ -4,24 +4,25 @@ package com.cybertek.tests.reviews;
 import lombok.AllArgsConstructor;
 
 
-@AllArgsConstructor
+//@AllArgsConstructor
 public class Users {
 
-    public String firstName;
-    public String lastName;
+
     public String username;
     public String password;
+    public String firstName;
+    public String lastName;
 
 
 
-//    public Users(String username, String password, String firstName, String lastName){
-//
-//        this.username=username;
-//        this.password=password;
-//        this.firstName=firstName;
-//        this.lastName=lastName;
-//
-//    }
+    public Users(String username, String password, String firstName, String lastName){
+
+        this.username=username;
+        this.password=password;
+        this.firstName=firstName;
+        this.lastName=lastName;
+
+    }
 
 
 }
